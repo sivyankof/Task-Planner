31bf6aa6736c75ec732643bfa8eabff5218966f2
Artem Sivyankov <morfiy0393@gmail.com>
2021-01-10 23:36:15 +0300

    добавил клик на кнопки (смена цветов) + скрывает/показывает список

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,34 @@
+let buttons = document.querySelector(".buttons-container");
+let lists = document.querySelector(`.list-container`);
 
+let selectedBtn;
+
+buttons.addEventListener(`click`, function (event) {
+    let target = event.target;
+
+    if (target.tagName != "BUTTON") {
+        return;
+    }
+
+    highlight(target);
+
+    showHideList(target);
+});
+
+function highlight(button) {
+    if (selectedBtn) {
+        selectedBtn.classList.remove("active");
+    }
+    selectedBtn = button;
+    selectedBtn.classList.add("active");
+}
+
+function showHideList(button) {
+    if (button == buttons.firstElementChild) {
+        lists.classList.add("show");
+        lists.classList.remove(`hide`);
+    } else {
+        lists.classList.add("hide");
+        lists.classList.remove(`show`);
+    }
+}
