8b745135f8c982ffe6a8b9b173d4ee11bba13873
Artem Sivyankov <morfiy0393@gmail.com>
2021-02-17 01:41:23 +0300

    fix: button tast, checkbox, localstrogate

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelector(".buttons-container");
 const lists = document.querySelector(`.list-container`);
 const li = document.getElementsByTagName(`li`);
-
+let ul = document.querySelector(`ul`);
 let body = document.querySelector(`body`);
 
 let selectedBtn;
@@ -10,21 +10,16 @@ let selectedBtn;
 buttons.addEventListener(`click`, function (event) {
     let target = event.target;
 
-    if (target.tagName != "BUTTON") {
-        return;
+    if (target.dataset.action == "show") {
+        target.classList.remove("active");
+        target.nextElementSibling.classList.add("active");
     }
 
-    highlight(target);
+    if (target.dataset.action == "hide") {
+        target.classList.remove("active");
+        target.previousElementSibling.classList.add("active");
+    }
 });
-
-//Функция меняет цвет кнопки в зависимости активна ли она или нет
-function highlight(button) {
-    if (selectedBtn) {
-        selectedBtn.classList.remove("active");
-    }
-    selectedBtn = button;
-    selectedBtn.classList.add("active");
-}
 
 // Добавление кнопки новой задачи
 let buttonAdd = document
@@ -37,9 +32,9 @@ let buttonAdd = document
             addTasks(target);
 
             let buttonSent = document.querySelector(".button-sent");
-            let ul = document.querySelector(`ul`);
             let inputTask = document.querySelector(".input-task");
 
+            //добавляю событие на кнопку
             inputTask.addEventListener("change", function (event) {
                 let li = document.createElement(`li`);
                 let input = document.createElement("input");
@@ -55,7 +50,7 @@ let buttonAdd = document
                 closeBlur.classList.add("closeBlur");
 
                 // реагирует на нажатие ПКМ
-                buttonSent.addEventListener("click", function (event) {
+                buttonSent.addEventListener("click", function () {
                     li.innerText = inputTask.value;
                     li.prepend(input);
                     li.append(closeButton);
@@ -103,19 +98,6 @@ function addTasks(button) {
     }
 }
 
-//сохраняем список в локале
-lists.addEventListener("DOMSubtreeModified", function () {
-    let ul = document.querySelector("ul");
-
-    localStorage.setItem("tasks", ul.innerHTML);
-});
-
-window.addEventListener("load", function () {
-    let ul = document.querySelector("ul");
-
-    ul.innerHTML = localStorage.getItem("tasks");
-});
-
 //Анимация через Class по сворачиванию списка. При клике на hide элементы (li) плавно перемещаются на в лево
 //(добавляя класс move)
 
@@ -128,7 +110,6 @@ buttons.addEventListener(`click`, function (event) {
                 e.classList.add(`move`);
             }
         }
-        // );
     } else if (target.dataset.action === `show`) {
         for (let e of li) {
             e.classList.remove(`move`);
@@ -139,30 +120,60 @@ buttons.addEventListener(`click`, function (event) {
 // при нажатии на li или checkbox текст перечеркивается и становиться серым
 lists.addEventListener("click", function (e) {
     let target = e.target;
-
+    // только при нажатии  на текст li
     if (target.tagName == "LI") {
         target.firstElementChild.checked
             ? ((target.firstElementChild.checked = false),
               target.firstElementChild.setAttribute("checked", false),
-              (target.style.textDecoration = "none"))
+              target.classList.remove("taskSelected"))
             : ((target.firstElementChild.checked = true),
               target.firstElementChild.setAttribute("checked", true),
-              (target.style.textDecoration = "line-through"));
-
-        target.style.color = target.firstElementChild.checked
-            ? `grey`
-            : `black`;
+              target.classList.add("taskSelected"));
     }
-
-    target.checked
-        ? ((target.parentNode.style.textDecoration = "line-through"),
-          target.firstElementChild.setAttribute("checked", true))
-        : ((target.parentNode.style.textDecoration = "none"),
-          target.firstElementChild.setAttribute("checked", false));
+    //только при нажатии на чекбокс
+    if (target.tagName == "INPUT") {
+        target.checked
+            ? (target.parentNode.classList.add("taskSelected"),
+              target.setAttribute("checked", true))
+            : (target.parentNode.classList.remove("taskSelected"),
+              target.setAttribute("checked", false));
+    }
 });
-
+//при нажатии на крестик удаляет ли
 lists.addEventListener("click", function (e) {
     if (e.target.className == "close") {
         e.target.parentNode.remove();
     }
+});
+
+// Наюблюдаем за изменением блока UL, для помещения актуальной инфо в local
+// Конфигурация observer (за какими изменениями наблюдать)
+const config = {
+    attributes: true,
+    childList: true,
+    subtree: true,
+};
+
+// Функция обратного вызова при срабатывании мутации
+const callback = function (mutationsList, observer) {
+    for (let mutation of mutationsList) {
+        if (mutation.type === "childList") {
+            //если был измененн тип, то добавляем его в локал
+            localStorage.setItem("tasks", ul.innerHTML);
+        } else if (mutation.type === "attributes") {
+            //если был измененн атрибут, то добавляем его в локал
+            localStorage.setItem("tasks", ul.innerHTML);
+        }
+    }
+};
+
+// Создаем экземпляр наблюдателя с указанной функцией обратного вызова
+const observer = new MutationObserver(callback);
+
+// Начинаем наблюдение за настроенными изменениями целевого элемента
+observer.observe(lists, config);
+
+//извлекаю задачи из локал
+window.addEventListener("load", function () {
+    ul.innerHTML = localStorage.getItem("tasks");
 });
